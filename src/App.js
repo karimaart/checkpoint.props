96bffc0e02894707d39a77659953346c9bb1453c
karimaart <rtibikarima20@gmail.com>
2023-09-26 12:53:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> TOP PRODUCTS </h1>
+      <div className="products-list">
+        <div className="card-item">
+          <h4>Casque Gamer SOG XPERT H600 7.1 VIRTUEL PC</h4>
+          <p>
+            Profitez d'une expérience à 360º grâce au son surround virtuel 7.1
+            du XPERT H600.
+          </p>
+          <img
+            src="https://www.scoop.com.tn/27507-large_default/casque-gamer-sog-xpert-h600-71-virtuel-pc.jpg"
+            alt=""
+            width="250px"
+          />
+          <h3>50 $</h3>
+        </div>
+        <div className="card-item">
+          <h4>MICRO CASQUE GAMER - Redragon H510 ZEUS-X RGB</h4>
+          <p>
+            Casque de jeu filaire RVB Coussinets d'oreille en similicuir de
+            qualité Son surround 7.1 Compatible avec PC/PS4/NS Bandeau réglable
+          </p>
+          <img
+            src="https://www.sbsinformatique.com/7050/tunisie/large/micro-casque-gamer-redragon-h510-zeus-x-rgb.jpg"
+            alt=""
+            width="250px"
+          />
+          <h3>70 $</h3>
+        </div>
+        <div className="card-item">
+          <h4>Casque Gaming XTRIKE ME GH707</h4>
+          <p>
+            Le micro-casque XTRIKE GH-707 propose aux joueurs une expérience
+            sonore agréable pour des parties pleines d'émotions.
+          </p>
+          <img
+            src="https://tunisiatech.tn/3573-large_default/casque-gaming-xtrike-me-gh707.jpg"
+            alt=""
+            width="250px"
+          />
+          <h3>50 $</h3>
+        </div>
+        <div className="card-item">
+          <h4>MICRO CASQUE GAMER VARR RGB USB BLACK VH8030</h4>
+          <p>
+            Dimensions du microphone : 6,0 x 5,0 mm Sensibilité : -54dB ± 3dB
+            Directivité : Omnidirectionnelle Impédance : 32 ± 15%
+          </p>
+          <img
+            src="https://mk-media.mytek.tn/media/catalog/product/cache/8be3f98b14227a82112b46963246dfe1/9/0/90yh02k0-b2ua00_1.jpg"
+            alt=""
+            width="250px"
+          />
+          <h3>40 $</h3>
+        </div>
+        <div className="card-item">
+          <h4>
+            MICRO CASQUE GAMER SPIRIT OF GAMER ELITE H60 – ROUGE (MIC-EH60)
+          </h4>
+          <p>
+            Connectivité: Filaire Réponse en fréquence: 20 Hz à 20KHz
+            Sensibilité Micro: – 40 dB (±3 dB) Diamètre de Haut-parleur: 50mm
+          </p>
+          <img
+            src="https://tunewtec.com/wp-content/uploads/2022/09/spirit-of-gamer-elite-h60-micro-casque-gamer-rouge.jpg"
+            alt=""
+            width="250px"
+          />
+          <h3>80 $</h3>
+        </div>
+        <div className="card-item">
+          <h4>MICRO CASQUE GAMER MEETION RGB MT-HP030</h4>
+          <p>
+            Micro Casque Gamer Meetion RGB MT-HP030 - Technologie de
+            Connectivité: USB - Réponse en fréquence: 10~20,000Hz - Longueur du
+            câble:
+          </p>
+          <img
+            src="https://spacenet.tn/111142-large_default/micro-casque-gamer-meetion-rgb-mt-hp030.jpg"
+            alt=""
+            width="250px"
+          />
+          <h3>50 $</h3>
+        </div>
+      </div>
     </div>
   );
+
 }
 
 export default App;
